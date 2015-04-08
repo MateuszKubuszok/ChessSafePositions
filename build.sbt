b9567a60fb4dc2d7c30245d94739c0ddf2c610f3
Mateusz Kubuszok <mkubuszok@opera.com>
2015-04-08 21:17:13 +0200

    Pieces definitions and tests

--- build.sbt
+++ build.sbt
@@ -4,6 +4,8 @@ lazy val root = (project in file(".")).settings(
 )
 
 libraryDependencies += "org.scala-lang.modules" %% "scala-java8-compat" % "0.2.0"
+libraryDependencies += "org.scalatest" %% "scalatest" % "2.2.4" % "test"
+libraryDependencies += "junit" % "junit" % "4.10" % "test"
 
 unmanagedSourceDirectories in Compile := (scalaSource in Compile).value :: Nil
 unmanagedSourceDirectories in Test    := (scalaSource in Test).value :: Nil
---
+++ src/main/scala/chess/Pieces.scala
@@ -0,0 +1,50 @@
+package chess
+
+case class Position(x: Int, y: Int) {
+  def xDiff(position: Position): Int = (x - position.x).abs
+    
+  def yDiff(position: Position): Int = (y - position.y).abs
+}
+
+abstract class Piece(occupies: Position) {
+  def isAttacking(position: Position): Boolean
+  
+  def isAttackingOrOccupies(position: Position): Boolean =
+    occupies == position || isAttacking(position) 
+}
+
+class King(occupies: Position) extends Piece(occupies) {
+  def isAttacking(position: Position): Boolean =
+    occupies.xDiff(position) <= 1 && occupies.yDiff(position) <= 1
+}
+
+class Knight(occupies: Position) extends Piece(occupies) {
+  def isAttacking(position: Position): Boolean =
+    (occupies.xDiff(position) == 1 && occupies.yDiff(position) == 2) ||
+    (occupies.xDiff(position) == 2 && occupies.yDiff(position) == 1)
+}
+
+trait RookLike {
+  def isAttackingLikeRook(occupies: Position, position: Position): Boolean =
+    occupies.xDiff(position) == 0 || occupies.yDiff(position) == 0
+}
+
+class Rook(occupies: Position) extends Piece(occupies) with RookLike {
+  def isAttacking(position: Position): Boolean =
+    isAttackingLikeRook(occupies, position)
+}
+
+trait BishopLike {
+  def isAttackingLikeBishop(occupies: Position, position: Position): Boolean =
+    occupies.xDiff(position) == occupies.yDiff(position)
+}
+
+class Bishop(occupies: Position) extends Piece(occupies) with BishopLike {
+  def isAttacking(position: Position): Boolean =
+    isAttackingLikeBishop(occupies, position)
+}
+
+class Queen(occupies: Position) extends Piece(occupies) with RookLike with BishopLike {
+  def isAttacking(position: Position): Boolean =
+    isAttackingLikeRook(occupies, position) || isAttackingLikeBishop(occupies, position)
+}
