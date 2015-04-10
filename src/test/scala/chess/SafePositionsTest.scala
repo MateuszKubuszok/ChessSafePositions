838dbfaf951a7605a384d7d6943c305525a2c633
Mateusz Kubuszok <mkubuszok@opera.com>
2015-04-10 21:42:40 +0200

    Achieved some solution, not sure if correct one

--- src/test/scala/chess/SafePositionsTest.scala
+++ src/test/scala/chess/SafePositionsTest.scala
@@ -14,10 +14,24 @@ class SafePositionsTests extends FunSuite {
     }
   }
   
+  test("On 2x2 board 5 King have 0 possible positions combinations") {
+    new SafePositions(2, 2, 5, 0, 0, 0, 0) {
+      assert(board.length == 4)
+      assert(solution.length == 0)
+    }
+  }
+  
   test("On 2x2 board 2 Knights can occupy one of 6 position combinations") {
     new SafePositions(2, 2, 0, 2, 0, 0, 0) {
       assert(board.length == 4)
       assert(solution.length == 6)
+    }
+  }
+  
+  test("On 7x7 board 2 Kings, 2 Queens, 2 Bishops and 1 Knights can occupy one of 357 position combinations") {
+    new SafePositions(7, 7, 2, 2, 2, 2, 1) {
+      assert(board.length == 49)
+      assert(solution.length == 357)
     }
   }
 }
